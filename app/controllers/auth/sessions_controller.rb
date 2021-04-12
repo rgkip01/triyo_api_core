ea5bb92a2ed42f8280aceeb0908d86233bc5527d
Rafael Abreu <web.nogs@gmail.com>
2021-04-12 14:21:22 -0300

    create logout to user session

--- app/controllers/auth/sessions_controller.rb
+++ app/controllers/auth/sessions_controller.rb
@@ -1,5 +1,7 @@
 class Auth::SessionsController < ApplicationController
-
+  before_action :authorize_access_request!, only: [:destroy]
+  before_action :authorize_refresh_request!, only: [:destroy_by_refresh]
+  
   def create
     user = User.find_by!(email: params[:email])
 
@@ -34,5 +36,21 @@ class Auth::SessionsController < ApplicationController
     else
       not_authorized
     end
+  end
+
+
+  def destroy
+    session = JWTSessions::Session.new(
+      payload: payload,
+      refresh_by_access_allowed: true
+    )
+    session.flush_by_access_payload
+    render json: :ok
+  end
+
+  def destroy_by_refresh
+    session = JWTSessions::Session.new(payload: payload)
+    session.flush_by_token(found_token)
+    render json: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  
   post '/users/new', to: 'users#create'
-
+  
   namespace :auth do 
     post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
   end
 end
